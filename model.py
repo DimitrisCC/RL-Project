461d8ad2166e1a1a0dffc3eb3aaaff8c0639e30e
Dimitris <dimitrispapatheodorou95@gmail.com>
2019-11-22 20:33:36 +0200

    new code

--- model.py
+++ model.py
@@ -1,267 +1,85 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-import numpy as np
-import random
+# -*- coding: utf-8 -*-
+from __future__ import division
 import math
-
-from functools import partial
-
-def Rainbow(env, args):
-    if args.c51:
-        if args.dueling:
-            model = CategoricalDuelingDQN(env, args.noisy, args.sigma_init,
-                                          args.Vmin, args.Vmax, args.num_atoms, args.batch_size)
-        else:
-            model = CategoricalDQN(env, args.noisy, args.sigma_init,
-                                   args.Vmin, args.Vmax, args.num_atoms, args.batch_size)
-    else:
-        if args.dueling:
-            model = DuelingDQN(env, args.noisy, args.sigma_init)
-        else:
-            model = DQNBase(env, args.noisy, args.sigma_init)
-            
-    return model
+import torch
+from torch import nn
+from torch.nn import functional as F
 
 
-class DQNBase(nn.Module):
-    """
-    Basic DQN + NoisyNet
-
-    Noisy Networks for Exploration
-    https://arxiv.org/abs/1706.10295
-    
-    parameters
-    ---------
-    env         environment(openai gym)
-    noisy       boolean value for NoisyNet. 
-                If this is set to True, self.Linear will be NoisyLinear module
-    """
-    def __init__(self, env, noisy, sigma_init):
-        super(DQNBase, self).__init__()
-        
-        self.input_shape = env.observation_space.shape
-        self.num_actions = env.action_space.n
-        self.noisy = noisy
-
-        if noisy:
-            self.Linear = partial(NoisyLinear, sigma_init=sigma_init)
-        else:
-            self.Linear = nn.Linear
-
-        self.flatten = Flatten()
-        
-        self.features = nn.Sequential(
-            nn.Conv2d(self.input_shape[0], 32, kernel_size=8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1),
-            nn.ReLU()
-        )
-        
-        self.fc = nn.Sequential(
-            self.Linear(self._feature_size(), 512),
-            nn.ReLU(),
-            self.Linear(512, self.num_actions)
-        )
-        
-    def forward(self, x):
-        x = self.features(x)
-        x = self.flatten(x)
-        x = self.fc(x)
-        return x
-    
-    def _feature_size(self):
-        return self.features(torch.zeros(1, *self.input_shape)).view(1, -1).size(1)
-    
-    def act(self, state, epsilon):
-        """
-        Parameters
-        ----------
-        state       torch.Tensor with appropritate device type
-        epsilon     epsilon for epsilon-greedy
-        """
-        if random.random() > epsilon or self.noisy:  # NoisyNet does not use e-greedy
-            with torch.no_grad():
-                state   = state.unsqueeze(0)
-                q_value = self.forward(state)
-                action  = q_value.max(1)[1].item()
-        else:
-            action = random.randrange(self.num_actions)
-        return action
-        
-    def update_noisy_modules(self):
-        if self.noisy:
-            self.noisy_modules = [module for module in self.modules() if isinstance(module, NoisyLinear)]
-    
-    def sample_noise(self):
-        for module in self.noisy_modules:
-            module.sample_noise()
-
-    def remove_noise(self):
-        for module in self.noisy_modules:
-            module.remove_noise()
-
-
-class DuelingDQN(DQNBase):
-    """
-    Dueling Network Architectures for Deep Reinforcement Learning
-    https://arxiv.org/abs/1511.06581
-    """
-    def __init__(self, env, noisy, sigma_init):
-        super(DuelingDQN, self).__init__(env, noisy, sigma_init)
-        
-        self.advantage = self.fc
-
-        self.value = nn.Sequential(
-            self.Linear(self._feature_size(), 512),
-            nn.ReLU(),
-            self.Linear(512, 1)
-        )
-    
-    def forward(self, x):
-        x = self.features(x)
-        x = self.flatten(x)
-        advantage = self.advantage(x)
-        value = self.value(x)
-        return value + advantage - advantage.mean(1, keepdim=True)
-
-
-class CategoricalDQN(DQNBase):
-    """
-    A Distributional Perspective on Reinforcement Learning
-    https://arxiv.org/abs/1707.06887
-    """
-
-    def __init__(self, env, noisy, sigma_init, Vmin, Vmax, num_atoms, batch_size):
-        super(CategoricalDQN, self).__init__(env, noisy, sigma_init)
-    
-        support = torch.linspace(Vmin, Vmax, num_atoms)
-        offset = torch.linspace(0, (batch_size - 1) * num_atoms, batch_size).long()\
-            .unsqueeze(1).expand(batch_size, num_atoms)
-
-        self.register_buffer('support', support)
-        self.register_buffer('offset', offset)
-        self.num_atoms = num_atoms
-
-        self.fc = nn.Sequential(
-            self.Linear(self._feature_size(), 512),
-            nn.ReLU(),
-            self.Linear(512, self.num_actions * self.num_atoms),
-        )
-
-        self.softmax = nn.Softmax(dim=1)
-    
-    def forward(self, x):
-        x = self.features(x)
-        x = self.flatten(x)
-        x = self.fc(x)
-        x = self.softmax(x.view(-1, self.num_atoms))
-        x = x.view(-1, self.num_actions, self.num_atoms)
-        return x
-    
-    def act(self, state, epsilon):
-        """
-        Parameters
-        ----------
-        state       torch.Tensor with appropritate device type
-        epsilon     epsilon for epsilon-greedy
-        """
-        if random.random() > epsilon or self.noisy:  # NoisyNet does not use e-greedy
-            with torch.no_grad():
-                state   = state.unsqueeze(0)
-                q_dist = self.forward(state)
-                q_value = (q_dist * self.support).sum(2)
-                action  = q_value.max(1)[1].item()
-        else:
-            action = random.randrange(self.num_actions)
-        return action
-
-
-class CategoricalDuelingDQN(CategoricalDQN):
-
-    def __init__(self, env, noisy, sigma_init, Vmin, Vmax, num_atoms, batch_size):
-        super(CategoricalDuelingDQN, self).__init__(env, noisy, sigma_init, Vmin, Vmax, num_atoms, batch_size)
-        
-        self.advantage = self.fc
-
-        self.value = nn.Sequential(
-            self.Linear(self._feature_size(), 512),
-            nn.ReLU(),
-            self.Linear(512, num_atoms)
-        )
-    
-    def forward(self, x):
-        x = self.features(x)
-        x = self.flatten(x)
-
-        advantage = self.advantage(x).view(-1, self.num_actions, self.num_atoms)
-        value = self.value(x).view(-1, 1, self.num_atoms)
-
-        x = value + advantage - advantage.mean(1, keepdim=True)
-        x = self.softmax(x.view(-1, self.num_atoms))
-        x = x.view(-1, self.num_actions, self.num_atoms)
-        return x
-
-
-class Flatten(nn.Module):
-    def forward(self, x):
-        return x.view(x.size(0), -1)
-
-
+# Factorised NoisyLinear layer with bias
 class NoisyLinear(nn.Module):
-    def __init__(self, in_features, out_features, sigma_init):
-        super(NoisyLinear, self).__init__()
-        
-        self.in_features = in_features
-        self.out_features = out_features 
-        self.sigma_init = sigma_init
+  def __init__(self, in_features, out_features, std_init=0.5):
+    super(NoisyLinear, self).__init__()
+    self.in_features = in_features
+    self.out_features = out_features
+    self.std_init = std_init
+    self.weight_mu = nn.Parameter(torch.empty(out_features, in_features))
+    self.weight_sigma = nn.Parameter(torch.empty(out_features, in_features))
+    self.register_buffer('weight_epsilon', torch.empty(out_features, in_features))
+    self.bias_mu = nn.Parameter(torch.empty(out_features))
+    self.bias_sigma = nn.Parameter(torch.empty(out_features))
+    self.register_buffer('bias_epsilon', torch.empty(out_features))
+    self.reset_parameters()
+    self.reset_noise()
 
-        self.weight_mu = nn.Parameter(torch.FloatTensor(out_features, in_features))
-        self.weight_sigma = nn.Parameter(torch.FloatTensor(out_features, in_features))
-        self.register_buffer('weight_epsilon', torch.FloatTensor(out_features, in_features))
+  def reset_parameters(self):
+    mu_range = 1 / math.sqrt(self.in_features)
+    self.weight_mu.data.uniform_(-mu_range, mu_range)
+    self.weight_sigma.data.fill_(self.std_init / math.sqrt(self.in_features))
+    self.bias_mu.data.uniform_(-mu_range, mu_range)
+    self.bias_sigma.data.fill_(self.std_init / math.sqrt(self.out_features))
 
-        self.bias_mu = nn.Parameter(torch.FloatTensor(out_features))
-        self.bias_sigma = nn.Parameter(torch.FloatTensor(out_features))
-        self.register_buffer('bias_epsilon', torch.FloatTensor(out_features))
+  def _scale_noise(self, size):
+    x = torch.randn(size)
+    return x.sign().mul_(x.abs().sqrt_())
 
-        self.register_buffer('sample_weight_in', torch.FloatTensor(in_features))
-        self.register_buffer('sample_weight_out', torch.FloatTensor(out_features))
-        self.register_buffer('sample_bias_out', torch.FloatTensor(out_features))
+  def reset_noise(self):
+    epsilon_in = self._scale_noise(self.in_features)
+    epsilon_out = self._scale_noise(self.out_features)
+    self.weight_epsilon.copy_(epsilon_out.ger(epsilon_in))
+    self.bias_epsilon.copy_(epsilon_out)
 
-        self.reset_parameters()
-        self.sample_noise()
-    
-    def forward(self, x):
-        if self.training:
-            weight = self.weight_mu + self.weight_sigma.mul(self.weight_epsilon)
-            bias = self.bias_mu + self.bias_sigma.mul(self.bias_epsilon)
-        else:
-            weight = self.weight_mu
-            bias = self.bias_mu
-        
-        return F.linear(x, weight, bias)
-    
-    def reset_parameters(self):
-        mu_range = 1 / math.sqrt(self.weight_mu.size(1))
+  def forward(self, input):
+    if self.training:
+      return F.linear(input, self.weight_mu + self.weight_sigma * self.weight_epsilon, self.bias_mu + self.bias_sigma * self.bias_epsilon)
+    else:
+      return F.linear(input, self.weight_mu, self.bias_mu)
 
-        self.weight_mu.data.uniform_(-mu_range, mu_range)
-        self.weight_sigma.data.fill_(self.sigma_init / math.sqrt(self.weight_sigma.size(1)))
 
-        self.bias_mu.data.uniform_(-mu_range, mu_range)
-        self.bias_sigma.data.fill_(self.sigma_init / math.sqrt(self.bias_sigma.size(0)))
+class DQN(nn.Module):
+  def __init__(self, args, action_space):
+    super(DQN, self).__init__()
+    self.atoms = args.atoms
+    self.action_space = action_space
 
-    def sample_noise(self):
-        self.sample_weight_in = self._scale_noise(self.sample_weight_in)
-        self.sample_weight_out = self._scale_noise(self.sample_weight_out)
-        self.sample_bias_out = self._scale_noise(self.sample_bias_out)
+    if args.architecture == 'canonical':
+      self.convs = nn.Sequential(nn.Conv2d(args.history_length, 32, 8, stride=4, padding=0), nn.ReLU(),
+                                 nn.Conv2d(32, 64, 4, stride=2, padding=0), nn.ReLU(),
+                                 nn.Conv2d(64, 64, 3, stride=1, padding=0), nn.ReLU())
+      self.conv_output_size = 3136
+    elif args.architecture == 'data-efficient':
+      self.convs = nn.Sequential(nn.Conv2d(args.history_length, 32, 5, stride=5, padding=0), nn.ReLU(),
+                                 nn.Conv2d(32, 64, 5, stride=5, padding=0), nn.ReLU())
+      self.conv_output_size = 576
+    self.fc_h_v = NoisyLinear(self.conv_output_size, args.hidden_size, std_init=args.noisy_std)
+    self.fc_h_a = NoisyLinear(self.conv_output_size, args.hidden_size, std_init=args.noisy_std)
+    self.fc_z_v = NoisyLinear(args.hidden_size, self.atoms, std_init=args.noisy_std)
+    self.fc_z_a = NoisyLinear(args.hidden_size, action_space * self.atoms, std_init=args.noisy_std)
 
-        self.weight_epsilon.copy_(self.sample_weight_out.ger(self.sample_weight_in))
-        self.bias_epsilon.copy_(self.sample_bias_out)
-    
-    def _scale_noise(self, x):
-        x = x.normal_()
-        x = x.sign().mul(x.abs().sqrt())
-        return x
+  def forward(self, x, log=False):
+    x = self.convs(x)
+    x = x.view(-1, self.conv_output_size)
+    v = self.fc_z_v(F.relu(self.fc_h_v(x)))  # Value stream
+    a = self.fc_z_a(F.relu(self.fc_h_a(x)))  # Advantage stream
+    v, a = v.view(-1, 1, self.atoms), a.view(-1, self.action_space, self.atoms)
+    q = v + a - a.mean(1, keepdim=True)  # Combine streams
+    if log:  # Use log softmax for numerical stability
+      q = F.log_softmax(q, dim=2)  # Log probabilities with action over second dimension
+    else:
+      q = F.softmax(q, dim=2)  # Probabilities with action over second dimension
+    return q
+
+  def reset_noise(self):
+    for name, module in self.named_children():
+      if 'fc' in name:
+        module.reset_noise()
